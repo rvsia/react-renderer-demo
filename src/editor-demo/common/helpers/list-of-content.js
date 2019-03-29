c77d38c0739e9380fc7edf3af1ebfac0d7eca35c
Richard Vsiansky <r.vsia@seznam.cz>
2019-03-29 11:34:41 +0100

    Adds table of content to form schemas page

--- src/editor-demo/common/helpers/list-of-content.js
+++ src/editor-demo/common/helpers/list-of-content.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactMarkdown from '../md-helper';
 
 export default ({ text }) => {
-  const regex = /###(.*)/gm;
+  const regex = /^### (.*)/gm;
   const found = text.match(regex);
   return <React.Fragment>
     <ReactMarkdown source={ '### Table of content' }/>
     { found.map((header, index) => (
-      <ReactMarkdown key={ header } source={ `[${header.replace('###', '')}](#${header.replace('### ', '').replace(' ', '-').toLowerCase()})` }/>
+      <ReactMarkdown key={ header } source={ `[${header.replace('###', '')}](#${header.replace('### ', '').replace(/ /g, '-').toLowerCase()})` }/>
     )) }
+    <hr/>
   </React.Fragment>;
 };
 
